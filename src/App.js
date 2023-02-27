ccf2054518328f0e925c2d0d611de756544beef6
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-27 11:12:41 -0300

    component lifecycle

--- src/App.js
+++ src/App.js
@@ -8,14 +8,67 @@ import Layout from "./components/Layout";
 import themes from "./styles/themes";
 
 class App extends React.Component {
+  state = {
+    changed: true,
+  };
+
+  // executed when component is mounted
+  componentDidMount() {
+    console.log("componentDidMount executed");
+  }
+
+  // executed before the component receives updates and renders
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate", {
+      currentState: this.state,
+      nextProps,
+      nextState,
+    });
+
+    // if true is returned, the component's lifecycle continues and executes the componentDidUpdate performing a new rendering
+    // if false is returned, rendering is blocked
+    return true;
+  }
+
+  // executed after the component receives updates and renders
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidMount updated", {
+      currentState: this.state,
+      prevState,
+      prevProps,
+    });
+  }
+
+  // when an error occurs in a child component
+  componentDidCatch(error, info) {
+    console.log("componentDidCatch", { error, info });
+  }
+
   render() {
     return (
       <ThemeProvider>
+        <button
+          onClick={() =>
+            this.setState((prevState) => ({ changed: !prevState.changed }))
+          }
+        >
+          Change State
+        </button>
         <ThemeContext.Consumer>
           {({ theme }) => (
             <StyledThemeProvider theme={themes[theme] || themes.dark}>
               <GlobalStyle />
-              <Layout />
+              {this.state.changed && <Layout />}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+              <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
             </StyledThemeProvider>
           )}
         </ThemeContext.Consumer>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,12 +4,31 @@ import Header from "../Header";
 import PostsList from "../PostsList";
 import Footer from "../Footer";
 
-export default function Layout() {
-  return (
-    <>
-      <Header />
-      <PostsList />
-      <Footer />
-    </>
-  );
+export default class Layout extends React.Component {
+  // executed when component is mounted
+  componentDidMount() {
+    document.addEventListener("scroll", this.handleScroll);
+  }
+
+  // executed when component is going to unmount
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    document.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    console.log("scrolled...");
+  };
+
+  render() {
+    return (
+      <>
+        {/* { simulating an error in the child component and receiving it in componentDidCatch} */}
+        {/* {undefined.map((item) => item)} */}
+        <Header />
+        <PostsList />
+        <Footer />
+      </>
+    );
+  }
 }

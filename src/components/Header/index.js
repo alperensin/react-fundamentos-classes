48be92e4435be294b74d731226eea9ce0d2071ac
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-26 22:10:07 -0300

    Header functional component converted to class component and receiving parameters

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import { Container } from "./styles";
 
-export default function Header({ selectedTheme, onToggleTheme }) {
-  return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <button onClick={onToggleTheme} type="button">
-        {selectedTheme === "dark" ? "🌞" : "🌚"}
-      </button>
-    </Container>
-  );
+export default class Header extends React.Component {
+  render() {
+    const { selectedTheme, onToggleTheme } = this.props;
+    return (
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button onClick={onToggleTheme} type="button">
+          {selectedTheme === "dark" ? "🌞" : "🌚"}
+        </button>
+      </Container>
+    );
+  }
 }

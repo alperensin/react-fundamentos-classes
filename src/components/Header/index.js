dd92852f1e462dfd2db2fae809775eeea4a3bbb1
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-27 10:26:49 -0300

    added propTypes on Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { Container } from "./styles";
 
 export default class Header extends React.Component {
+  static propTypes = {
+    selectedTheme: PropTypes.string.isRequired,
+    onToggleTheme: PropTypes.func.isRequired,
+  };
+
   render() {
     const { selectedTheme, onToggleTheme } = this.props;
     return (
